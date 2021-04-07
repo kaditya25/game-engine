68174eb68281f8a5dc816ab18af8ab0246af3021
Todd Humphreys <todd.humphreys@utexas.edu>
2021-04-07 13:30:49 -0500

    Fixed more compiler warnings.

--- src/ros/node/balloon_status_subscriber_node.cc
+++ src/ros/node/balloon_status_subscriber_node.cc
@@ -1,11 +1,9 @@
-// Author: Tucker Haydon
-
 #include "balloon_status_subscriber_node.h"
 
 namespace game_engine {
-  BalloonStatusSubscriberNode::BalloonStatusSubscriberNode(
-      const std::string& topic, 
-      std::shared_ptr<BalloonStatus> balloon_status) {
+  BalloonStatusSubscriberNode::
+  BalloonStatusSubscriberNode(const std::string& topic, 
+                              std::shared_ptr<BalloonStatus> balloon_status) {
     this->balloon_status_ = balloon_status;
     this->node_handle_ = ros::NodeHandle("/game_engine/");
     this->subscriber_ = node_handle_.subscribe(
@@ -15,18 +13,19 @@ namespace game_engine {
         this);
   }
 
-  void BalloonStatusSubscriberNode::SubscriberCallback(const mg_msgs::BalloonStatus& msg) {
+  void BalloonStatusSubscriberNode::
+  SubscriberCallback(const mg_msgs::BalloonStatus& msg) {
     Eigen::Vector3d position;
     position[0] = msg.pos.x;
     position[1] = msg.pos.y;
     position[2] = msg.pos.z;
 
     BalloonStatus balloon_status {
-      .popped = msg.popped.data,
+      .popped = static_cast<bool>(msg.popped.data),
       .popper = msg.popper.data,
       .pop_time = msg.pop_time.data,
       .position = position,
-      .set_start = msg.set_start.data
+      .set_start = static_cast<bool>(msg.set_start.data)
     };
     *(this->balloon_status_) = balloon_status;
   }
