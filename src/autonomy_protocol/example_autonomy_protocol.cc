212898f4efc1cf070b2eddaf00dadede6ce5bb90
Todd Humphreys <todd.humphreys@utexas.edu>
2021-05-03 16:53:41 -0500

    Improved example_autonomy_protocol.cc

--- src/autonomy_protocol/example_autonomy_protocol.cc
+++ src/autonomy_protocol/example_autonomy_protocol.cc
@@ -15,47 +15,90 @@
 
 namespace game_engine {
   std::chrono::milliseconds dt_chrono = std::chrono::milliseconds(40);
+
+
+  // UpdateTrajectories creates and returns a proposed trajectory.  The
+  // proposed trajectory gets submitted to the mediation_layer (ML), which
+  // responds by setting the data member trajectoryCode_.  See the header file
+  // game-engine/src/util/trajectory_codes.h for a list of possible codes.
+  //
+  // Any code other than TrajectoryCode::Success indicates that the ML has
+  // rejected the submitted trajectory.
+  //
+  // trajectoryCode_ is initialized with TrajectoryCode::Success, so this will
+  // be its value the first time this function is called (before any
+  // trajectories have been submitted).  Thereafter, trajectoryCode_ will
+  // indicate the TrajectoryCode for the most recently submitted trajectory.
   std::unordered_map<std::string, Trajectory>
   ExampleAutonomyProtocol::UpdateTrajectories() {
 
-    // This function creates and returns a proposed trajectory.  The
-    // trajectory gets submitted to the mediation_layer (ML), which responds
-    // by setting the data member trajectoryCode_.  See the header file
-    // game-engine/src/util/trajectory_codes.h for a list of possible codes.
-    //
-    // Any code other than TrajectoryCode::Success indicates that the ML has
-    // rejected the submitted trajectory.
-    //
-    // trajectoryCode_ is initialized with TrajectoryCode::Success, so this
-    // will be its value the first time this function is called (before any
-    // trajectories have been submitted).  Thereafter, trajectoryCode_ will
-    // indicate the response code for the most recently submitted trajectory.
+    // Set duration of example trajectory
+    constexpr int duration_sec = 30;
+    const std::chrono::milliseconds T_chrono =
+      std::chrono::seconds(duration_sec);
 
+    // 'static' variables act like Matlab persistent variables, maintaining
+    // their value between function calls. The initializer is only called
+    // once.
     static OccupancyGrid3D occupancy_grid;
-    static Graph3D graphOfArena; // Used by A*, you'll need to convert A* 2D to 3D
+    static Graph3D graphOfArena; 
     static Student_game_engine_visualizer visualizer;
     static bool firstTime = true;
-    static std::string& quad_name = friendly_names_[0];
-    
+    static Eigen::Vector3d start_pos;
+    static const std::chrono::time_point<std::chrono::system_clock> start_chrono_time
+      = std::chrono::system_clock::now();
+    static const std::chrono::time_point<std::chrono::system_clock> end_chrono_time
+      = start_chrono_time + T_chrono;
+    static bool arrived_at_goal = false;
+
+    // Load the current quad position
+    const std::string& quad_name = friendly_names_[0];
+    Eigen::Vector3d current_pos;
+    snapshot_->Position(quad_name, current_pos);
+
+    // Configure some variables the first time this function is called
     if(firstTime) {
       firstTime = false;
       occupancy_grid.LoadFromMap(map3d_, DISCRETE_LENGTH, SAFETY_BOUNDS);
-      graphOfArena = occupancy_grid.AsGraph();  // You can run A* on this graph
+      // You can run A* on graphOfArena once you created a 3D version of A*
+      graphOfArena = occupancy_grid.AsGraph();  
       visualizer.startVisualizing("/game_engine/environment");
+      start_pos = current_pos;
     }
 
-    // Obtain current balloon positions and popped state
+    // Obtain current balloon positions and popped states
     const Eigen::Vector3d red_balloon_pos = red_balloon_status_->position;
     const Eigen::Vector3d blue_balloon_pos = blue_balloon_status_->position;
     const bool red_balloon_popped = red_balloon_status_->popped;
     const bool blue_balloon_popped = blue_balloon_status_->popped;
 
-    // If you want to see a numerical value for the code, you can cast and
-    // print the code as shown below.
-    if (trajectoryCode_ != TrajectoryCode::Success) {
-      std::cout << "Response code: " <<
-        static_cast<unsigned int>(trajectoryCode_) << std::endl;
+    // Condition some decisions on wind intensity
+    switch (wind_intensity_) {
+    case WindIntensity::Mild:
+      // Do something mild
+      break;
+    case WindIntensity::Stiff:
+      // Do something stiff
+      break;
+    case WindIntensity::Intense:
+      // Do something intense
+      break;
+    case WindIntensity::Ludicrous:
+      // Do something ludicrous
+      break;
+    default:
+      std::cerr << "Unrecognized WindIntenstiy" << std::endl;
+      std::exit(EXIT_FAILURE);
     }
+    
+    // Respond to anomalous TrajectoryCode
+    if (trajectoryCode_ != TrajectoryCode::Success) {
+      
+      // If you want to see a numerical value for the code, you can cast and
+      // print the code as shown below.
+      std::cout << "TrajectoryCode: " <<
+        static_cast<int>(trajectoryCode_) << std::endl;
+    }   
 
     // Suppose your AP initially submits a trajectory with a time that exceeds
     // the maximum allowed time between points. You could apply a conditional
@@ -69,20 +112,12 @@ namespace game_engine {
 
     // Always use the chrono::system_clock for time. Trajectories require time
     // points measured in floating point seconds from the unix epoch.
-    const std::chrono::milliseconds T_chrono = std::chrono::seconds(30);
     const double dt =
       std::chrono::duration_cast<std::chrono::duration<double>>(dt_chrono).count();
-
-    // Use a static function variable to log the first time this function was
-    // called. The static function variable acts like a matlab persistent
-    // variable, maintaining its value between function calls. The initializer
-    // is only called once.
-    static const std::chrono::time_point<std::chrono::system_clock> start_chrono_time
-      = std::chrono::system_clock::now();
-    static const std::chrono::time_point<std::chrono::system_clock> end_chrono_time
-      = start_chrono_time + T_chrono;
     const std::chrono::time_point<std::chrono::system_clock> current_chrono_time
       = std::chrono::system_clock::now();
+    const std::chrono::duration<double> remaining_chrono_time =
+      end_chrono_time - current_chrono_time;
 
     // The following code generates and returns a new trajectory each time it
     // runs.  The new trajectory starts at the location on the original circle
@@ -94,18 +129,29 @@ namespace game_engine {
     // the greatest distance in the allotted time in the presence of
     // disturbance accelerations.
 
-    const std::chrono::duration<double> remaining_chrono_time =
-      end_chrono_time - current_chrono_time;
-
     // Number of samples
     const size_t N = remaining_chrono_time/dt_chrono;
 
-    // If end time has been exceeded, or if there are too few samples for a
-    // trajectory, return an empty map
-    constexpr size_t min_required_samples_in_trajectory = 2;
+    // Create an empty map
     std::unordered_map<std::string, Trajectory> trajectory_map;
-    if(current_chrono_time >= end_chrono_time ||
+    
+    // If end time has been exceeded, or if there are too few samples for a
+    // valid trajectory, return an empty map
+    constexpr size_t min_required_samples_in_trajectory = 2;
+    if (current_chrono_time >= end_chrono_time ||
        N < min_required_samples_in_trajectory) {
+      return trajectory_map;
+    }    
+    
+    // If quad has arrived at goal position, halt there by returning an empty
+    // map
+    constexpr double goal_arrival_threshold = 0.1;
+    const Eigen::Vector3d dv = current_pos - goal_position_;
+    if (remaining_chrono_time < std::chrono::seconds(duration_sec - 10) &&
+       dv.norm() < goal_arrival_threshold) {
+      arrived_at_goal = true;
+    }
+    if (arrived_at_goal) {
       return trajectory_map;
     }
 
@@ -113,16 +159,11 @@ namespace game_engine {
     const double radius = 0.5;
 
     // Angular speed in radians/s
-    constexpr double omega = 2*M_PI/30;
-
-    // Load the current position
-    Eigen::Vector3d current_pos;
-    this->snapshot_->Position(quad_name, current_pos);
+    constexpr double omega = 2*M_PI/duration_sec;
 
     // Place the center of the circle 1 radius in the y-direction from the
     // starting position
-    static Eigen::Vector3d circle_center =
-      current_pos + Eigen::Vector3d(0, radius, 0);
+    Eigen::Vector3d circle_center = start_pos + Eigen::Vector3d(0, radius, 0);
 
     // Transform the current position into an angle
     const Eigen::Vector3d r = current_pos - circle_center;
