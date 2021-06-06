2851c1452295b210c789c94c1f6433e061f372c1
Hailey Nichols <haileyn@utexas.edu>
2021-06-06 17:09:44 -0500

    re-arranging files to separate aerial robotics stuff from machine games stuff

---
+++ src/autonomy_protocol/machine_games/red_team_autonomy_protocol.cc
@@ -0,0 +1,12 @@
+#include "red_team_autonomy_protocol.h"
+
+namespace game_engine {
+
+    std::unordered_map<std::string, Trajectory>
+    RedTeamAutonomyProtocol::UpdateTrajectories() {
+
+        // RED TEAM: FILL OUT THIS FUNCTION
+
+        return std::unordered_map<std::string, Trajectory>();
+    }
+}
---
+++ src/autonomy_protocol/machine_games/red_team_autonomy_protocol.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "autonomy_protocol.h"
+
+namespace game_engine {
+    class RedTeamAutonomyProtocol : public AutonomyProtocol {
+    private:
+
+    public:
+        RedTeamAutonomyProtocol(
+                const std::vector<std::string>& friendly_names,
+                const std::vector<std::string>& enemy_names,
+                const std::shared_ptr<GameSnapshot> snapshot,
+                const std::shared_ptr<TrajectoryWardenClient> trajectory_warden_client,
+                const std::shared_ptr<PreSubmissionTrajectoryVetter> prevetter,
+                const Map3D& map3d,
+                const std::shared_ptr<BalloonStatus> red_balloon_status,
+                const std::shared_ptr<BalloonStatus> blue_balloon_status,
+                const Eigen::Vector3d& goal_position,
+                const WindIntensity& wind_intensity)
+                : AutonomyProtocol(
+                friendly_names,
+                enemy_names,
+                snapshot,
+                trajectory_warden_client,
+                prevetter,
+                map3d,
+                red_balloon_status,
+                blue_balloon_status,
+                goal_position,
+                wind_intensity) {}
+
+        std::unordered_map<std::string, Trajectory> UpdateTrajectories() override;
+    };
+}
