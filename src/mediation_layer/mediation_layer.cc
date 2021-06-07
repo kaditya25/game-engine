ef3f0b9816b55aa8f1d2769a506a57db7b6ce60f
Hailey Nichols <haileyn@utexas.edu>
2021-06-07 17:21:10 -0500

    now works for freezing multiple quads

--- src/mediation_layer/mediation_layer.cc
+++ src/mediation_layer/mediation_layer.cc
@@ -12,7 +12,7 @@ namespace game_engine {
        std::shared_ptr<TrajectoryWardenPublisher> trajectory_warden_pub,
        std::shared_ptr<QuadStateWarden> quad_state_warden,
        std::shared_ptr<QuadStateWatchdogStatus> quad_state_watchdog_status,
-       std::unordered_map<std::string, std::shared_ptr<TrajectoryPublisherNode>> trajectory_publishers) {
+       std::shared_ptr<TrajectoryPublisherNode> publisher) {
 
       TrajectoryVetter trajectory_vetter;
       while(true == this->ok_) {
@@ -23,7 +23,7 @@ namespace game_engine {
           QuadState current_quad_state;
           quad_state_warden->Read(key, current_quad_state);
 
-          static const Eigen::Vector3d freeze_quad_position = current_quad_state.Position();
+          const Eigen::Vector3d freeze_quad_position = current_quad_state.Position();
 
           std::cerr
             << key 
@@ -49,7 +49,7 @@ namespace game_engine {
           }
           const Trajectory freeze_trajectory(freeze_trajectory_vector);
           
-          trajectory_warden_pub->Write(key, freeze_trajectory_vector, trajectory_publishers);
+          trajectory_warden_pub->Write(key, freeze_trajectory_vector, publisher);
           std::this_thread::sleep_for(std::chrono::milliseconds(1000));
         }
 
@@ -70,7 +70,7 @@ namespace game_engine {
                       "." << std::endl;
             continue;
           }
-          trajectory_warden_pub->Write(key, trajectory, trajectory_publishers);
+          trajectory_warden_pub->Write(key, trajectory, publisher);
         }
     }
   }
@@ -98,7 +98,7 @@ namespace game_engine {
                        trajectory_warden_pub,
                        quad_state_warden,
                        quad_state_watchdog_status,
-                       trajectory_publishers);})));
+                       trajectory_publishers[key]);})));
     }
 
     // Wait for this thread to receive a stop command
