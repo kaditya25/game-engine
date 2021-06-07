986149ece761a686cbd71d0e5b58691e0039f446
Hailey Nichols <haileyn@utexas.edu>
2021-06-07 17:20:06 -0500

    checks if quads are too close together and reports an infrigement

--- src/mediation_layer/quad_state_watchdog.cc
+++ src/mediation_layer/quad_state_watchdog.cc
@@ -16,27 +16,44 @@ namespace game_engine {
     // have been expanded by min_distance and whose boundaries have been shrunk
     // by min_distance. After inflating the map, the quadcopter may be treated
     // as a point-particle. To determine if the quadcopter is within an
-    // l-infinity distance of min_distance from any obstacle, simpy check
+    // l-infinity distance of min_distance from any obstacle, simply check
     // whether the quad's center point is intersecting any of the inflate
-    // obstacles.
+    // obstacles. The distance between quads is also determined and a violation
+    // is reported if the quads get too close.
     const Map3D inflated_map = map.Inflate(this->options_.min_distance);
 
     while(this->ok_) {
       for(const std::string& quad_name: quad_names) {
-        // Read in the curret state
+        // Read in the current state
         QuadState current_state;
         quad_state_warden->Read(quad_name, current_state);
 
-        // Get the current position
+        // Get the current position of the quad
         Eigen::Vector3d current_position = current_state.Position();
 
         // Evaluate whether the current position intersects an obstacle
-        bool infraction_occurred = 
-             !inflated_map.IsFreeSpace(current_position) 
-          || !inflated_map.Contains(current_position);
-	
+        bool infraction_occurred =
+            !inflated_map.IsFreeSpace(current_position)
+            || !inflated_map.Contains(current_position);
+
         if(true == infraction_occurred) {
           quad_state_watchdog_status->Write(quad_name, true);
+        }
+
+        // Check if current quad too close to another quad
+        if (quad_names.size() > 1) {
+          for(const std::string& other_quad_name: quad_names) {
+            if(quad_name != other_quad_name) {
+              // Grab position of other quad
+              QuadState other_quad_current_state;
+              quad_state_warden->Read(other_quad_name, other_quad_current_state);
+              Eigen::Vector3d other_quad_current_position = other_quad_current_state.Position();
+              // Check if distance between quads is less than the limit
+              if((other_quad_current_position - current_position).norm() < this->options_.min_distance_btwn_quads) {
+                quad_state_watchdog_status->Write(quad_name, true);
+              }
+            }
+          }
         }
       }
       // 20 Hz
