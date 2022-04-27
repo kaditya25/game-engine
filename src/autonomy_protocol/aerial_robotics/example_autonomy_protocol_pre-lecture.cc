eb75a405c3196aa49189e6470ffdb57bfa7c2079
Robert <RMTenny@utexas.edu>
2022-04-27 17:19:24 -0500

    added files for lecture

--- src/autonomy_protocol/aerial_robotics/example_autonomy_protocol_pre-lecture.cc
+++ src/autonomy_protocol/aerial_robotics/example_autonomy_protocol_pre-lecture.cc
@@ -8,7 +8,7 @@
 // The length of one side of a cube in meters in the occupancy grid
 #define DISCRETE_LENGTH .2
 // How big the "inflation" bubble around obstacles will be, in meters
-#define SAFETY_BOUNDS .34  
+#define SAFETY_BOUNDS .34
 
 namespace game_engine {
   std::chrono::milliseconds dt_chrono = std::chrono::milliseconds(40);
@@ -25,13 +25,13 @@ namespace game_engine {
   // be its value the first time this function is called (before any
   // trajectories have been submitted).  Thereafter, trajectoryCodeMap_ will
   // indicate the MediationLayerCode for the most recently submitted trajectory.
-  std::unordered_map<std::string, Trajectory>
+  // TODO: Determine Return type
   ExampleAutonomyProtocol::UpdateTrajectories() {
 
     // Set the duration of the example trajectory
     constexpr int duration_sec = 30;
     const std::chrono::milliseconds T_chrono =
-      std::chrono::seconds(duration_sec);
+            std::chrono::seconds(duration_sec);
 
     // 'static' variables act like Matlab persistent variables, maintaining
     // their value between function calls. The initializer is only called
@@ -48,96 +48,97 @@ namespace game_engine {
     static bool halt = false;
     static Eigen::Vector3d start_pos;
     static const std::chrono::time_point<std::chrono::system_clock> start_chrono_time
-      = std::chrono::system_clock::now();
+            = std::chrono::system_clock::now();
     static const std::chrono::time_point<std::chrono::system_clock> end_chrono_time
-      = start_chrono_time + T_chrono;
+            = start_chrono_time + T_chrono;
 
     // Load the current quad position
-    const std::string& quad_name = friendly_names_[0];
+    const std::string &quad_name = friendly_names_[0];
     Eigen::Vector3d current_pos;
     snapshot_->Position(quad_name, current_pos);
 
     // Set some static variables the first time this function is called
     if (firstTime) {
-      firstTime = false;
-      occupancy_grid.LoadFromMap(map3d_, DISCRETE_LENGTH, SAFETY_BOUNDS);
-      // You can run A* on graphOfArena once you created a 3D version of A*
-      graphOfArena = occupancy_grid.AsGraph();  
-      visualizer.startVisualizing("/game_engine/environment");
-      start_pos = current_pos;
+      // TODO: Add setup for inilization
     }
 
     // Obtain current balloon positions and popped states
-    const Eigen::Vector3d red_balloon_pos = *red_balloon_position_;
-    const Eigen::Vector3d blue_balloon_pos = *blue_balloon_position_;
-    const bool red_balloon_popped = red_balloon_status_->popped;
-    const bool blue_balloon_popped = blue_balloon_status_->popped;
+    // TODO: Obtain positoin of red_balloon_pos
+    //                          blue_balloon_pos
+    // TODO: Obtain balloon states red_balloon_popped
+    //                             blue_balloon_popped
+
     // Draws estimates of the Balloon Position (if you are pulling in the real values, the spheres will be
     // overlaid on the balloons). While the balloon is not popped, we will keep displaying
     // the sphere until the balloon is popped. The red balloon position is a pink sphere and the blue balloon position
     // is a turquoise sphere.
-    if(!red_balloon_popped) {
+    if (!red_balloon_popped) {
       visualizer.drawBalloonPosition(red_balloon_pos, "red");
     }
-    if(!blue_balloon_popped) {
+    if (!blue_balloon_popped) {
       visualizer.drawBalloonPosition(blue_balloon_pos, "blue");
     }
 
     // Condition some decisions on wind intensity
     switch (wind_intensity_) {
-    case WindIntensity::Zero:
-      // Do something zero-ish
-      break;
-    case WindIntensity::Mild:
-      // Do something mild
-      break;
-    case WindIntensity::Stiff:
-      // Do something stiff
-      break;
-    case WindIntensity::Intense:
-      // Do something intense
-      break;
-    case WindIntensity::Ludicrous:
-      // Do something ludicrous
-      break;
-    default:
-      std::cerr << "Unrecognized WindIntensity value." << std::endl;
-      std::exit(EXIT_FAILURE);
+      case WindIntensity::Zero:
+        // Do something zero-ish
+        break;
+      case WindIntensity::Mild:
+        // Do something mild
+        break;
+      case WindIntensity::Stiff:
+        // Do something stiff
+        break;
+      case WindIntensity::Intense:
+        // Do something intense
+        break;
+      case WindIntensity::Ludicrous:
+        // Do something ludicrous
+        break;
+      default:
+        std::cerr << "Unrecognized WindIntensity value." << std::endl;
+        std::exit(EXIT_FAILURE);
     }
 
     // You can fill out this switch statement with case statements tailored to
     // each MediationLayerCode.
     switch (trajectoryCodeMap_[quad_name].code) {
-    case MediationLayerCode::Success:
-      // You probably won't need to do anything in response to Success.
-      break;
-    case MediationLayerCode::TimeBetweenPointsExceedsMaxTime: {
-      // Suppose your AP initially submits a trajectory with a time that
-      // exceeds the maximum allowed time between points. You could fix the
-      // problem as shown below.
-      std::cout << "Replanning trajectory: "
-        "Shortening time between trajectory points." << std::endl;
-      std::cout << "Value: " << trajectoryCodeMap_[quad_name].value << std::endl;
-      dt_chrono = dt_chrono - std::chrono::milliseconds(15);
-      break;
+      case MediationLayerCode::Success:
+        // You probably won't need to do anything in response to Success.
+        break;
+      case MediationLayerCode::TimeBetweenPointsExceedsMaxTime: {
+        // Suppose your AP initially submits a trajectory with a time that
+        // exceeds the maximum allowed time between points. You could fix the
+        // problem as shown below.
+        std::cout << "Replanning trajectory: "
+                     "Shortening time between trajectory points." << std::endl;
+        std::cout << "Value: " << trajectoryCodeMap_[quad_name].value
+                  << std::endl;
+        dt_chrono = dt_chrono - std::chrono::milliseconds(15);
+        break;
+      }
+      default:
+        // If you want to see a numerical MediationLayerCode value, you can cast and
+        // print the code as shown below.
+        std::cout << "MediationLayerCode: " <<
+                  static_cast<int>(trajectoryCodeMap_[quad_name].code)
+                  << std::endl;
+        std::cout << "Value: " << trajectoryCodeMap_[quad_name].value
+                  << std::endl;
+        std::cout << "Index: " << trajectoryCodeMap_[quad_name].index
+                  << std::endl;
     }
-    default:
-      // If you want to see a numerical MediationLayerCode value, you can cast and
-      // print the code as shown below.
-      std::cout << "MediationLayerCode: " <<
-        static_cast<int>(trajectoryCodeMap_[quad_name].code) << std::endl;
-      std::cout << "Value: " << trajectoryCodeMap_[quad_name].value << std::endl;
-      std::cout << "Index: " << trajectoryCodeMap_[quad_name].index << std::endl;
-    }
-    
+
     // Always use the chrono::system_clock for time. Trajectories require time
     // points measured in floating point seconds from the Unix epoch.
     const double dt =
-      std::chrono::duration_cast<std::chrono::duration<double>>(dt_chrono).count();
+            std::chrono::duration_cast<std::chrono::duration<double>>(
+                    dt_chrono).count();
     const std::chrono::time_point<std::chrono::system_clock> current_chrono_time
-      = std::chrono::system_clock::now();
+            = std::chrono::system_clock::now();
     const std::chrono::duration<double> remaining_chrono_time =
-      end_chrono_time - current_chrono_time;
+            end_chrono_time - current_chrono_time;
 
     // The following code generates and returns a new trajectory each time it
     // runs.  The new trajectory starts at the location on the original circle
@@ -150,58 +151,47 @@ namespace game_engine {
     // disturbance accelerations.
 
     // Number of samples
-    const size_t N = remaining_chrono_time/dt_chrono;
+    const size_t N = remaining_chrono_time / dt_chrono;
 
     // Create an empty map
-    std::unordered_map<std::string, Trajectory> trajectory_map;
-    
+    // TODO: Create empty map
+
     // If end time has been exceeded, or if there are too few samples for a
     // valid trajectory, return an empty map
     constexpr size_t min_required_samples_in_trajectory = 2;
     if (current_chrono_time >= end_chrono_time ||
-       N < min_required_samples_in_trajectory) {
+        N < min_required_samples_in_trajectory) {
       return trajectory_map;
     }
 
     // Halt at goal position when close enough
-    constexpr double goal_arrival_threshold_meters = 0.3;
+    // TODO: Set Goal Position threshold
     const Eigen::Vector3d dv = current_pos - goal_position_;
     // TrajectoryVector3D is an std::vector object defined in trajectory.h
-    TrajectoryVector3D trajectory_vector;
+    // TODO: Define new Trajectory Vector 3d
     if (halt ||
         (remaining_chrono_time < std::chrono::seconds(duration_sec - 10) &&
          dv.norm() < goal_arrival_threshold_meters)) {
       halt = true;
       for (size_t idx = 0; idx < 20; ++idx) {
         const std::chrono::duration<double> flight_chrono_time
-          = current_chrono_time.time_since_epoch() + idx * dt_chrono;
+                = current_chrono_time.time_since_epoch() + idx * dt_chrono;
         const double flight_time = flight_chrono_time.count();
-        trajectory_vector.push_back((Eigen::Matrix<double, 11, 1>() <<
-                                     goal_position_.x(),
-                                     goal_position_.y(),
-                                     goal_position_.z(),
-                                     0,0,0,
-                                     0,0,0,
-                                     0,
-                                     flight_time).finished());
+        // TODO: Pushback trajectory
       }
-      Trajectory trajectory(trajectory_vector);
-      visualizer.drawTrajectory(trajectory);
-      trajectory_map[quad_name] = trajectory;
-      return trajectory_map;
+      // TODO: return this trajectory
     }
-
     // Create Circular Trajectory
     // Set the radius of the circular example trajectory in meters
-    const double radius = 0.75;
+    // TODO: set circle radius
 
     // Set the angular rate of the circular example trajectory in radians/s 
-    constexpr double omega = 2*M_PI/duration_sec;
+    // TODO: Set angular rate
 
     // Place the center of the circle 1 radius in the y-direction from the
     // goal position
     const Eigen::Vector3d circle_center =
-      goal_position_ + Eigen::Vector3d(0, radius, 0);
+            goal_position_ + Eigen::Vector3d(0, radius, 0);
 
     // Transform the current position into an angle
     const Eigen::Vector3d r = current_pos - circle_center;
@@ -212,7 +202,7 @@ namespace game_engine {
       // chrono::duration<double> maintains high-precision floating point time
       // in seconds use the count function to cast into floating point
       const std::chrono::duration<double> flight_chrono_time
-        = current_chrono_time.time_since_epoch() + idx * dt_chrono;
+              = current_chrono_time.time_since_epoch() + idx * dt_chrono;
 
       // Angle in radians
       const double theta = theta_start + omega * idx * dt;
@@ -224,7 +214,7 @@ namespace game_engine {
 
       // Generate velocities via Chain rule
       const double vx = -radius * std::sin(theta) * omega;
-      const double vy =  radius * std::cos(theta) * omega;
+      const double vy = radius * std::cos(theta) * omega;
       const double vz = 0.0;
 
       // generate accelerations using Chain rule
@@ -239,16 +229,10 @@ namespace game_engine {
       const double flight_time = flight_chrono_time.count();
 
       // Push an Eigen instance onto the trajectory vector
-      trajectory_vector.push_back(
-          (Eigen::Matrix<double, 11, 1>() <<
-            x,   y,   z,
-            vx,  vy,  vz,
-            ax,  ay,  az,
-            yaw,
-            flight_time).finished());
-    }
+      // TODO: Push Eigen instance into trajectory vector
+      }
     // Construct a trajectory from the trajectory vector
-    Trajectory trajectory(trajectory_vector);
+    // TODO: Call trajectory constructor
 
     // Before submission of the trajectory, you can use this prevetting interface to determine if the trajectory
     // violates any mediation layer enforced constraints. This interface can be found in
@@ -259,22 +243,29 @@ namespace game_engine {
     // 3) The failure index. This is the violation index that caused the mediation layer to kick back an error code.
     //    The index is the sampled index (specified from the P4 sampling process), so you can figure out the
     //    waypoint index by a simple math transformation.
-    TrajectoryCode check_trajectory_before_submission = prevetter_->PreVet(quad_name, trajectory, map3d_);
+
+    // TODO: get prevetter codes
     switch (check_trajectory_before_submission.code) {
-        case MediationLayerCode::Success:
-            // You probably won't need to do anything in response to Success.
-            break;
-        case MediationLayerCode::TimeBetweenPointsExceedsMaxTime: {
-            // Suppose your AP intends to submit a trajectory with a time that exceeds the maximum allowed time
-            // between points. The prevetter would catch this before you submit to the mediation layer.
-            std::cout << "Prevet: Shorten time between trajectory points." << std::endl;
-            std::cout << "Prevet: Time violation: " << check_trajectory_before_submission.value << std::endl;
-            break;
-        }
-        default:
-            std::cout << "Prevet code: " << static_cast<int>(check_trajectory_before_submission.code) << std::endl;
-            std::cout << "Prevet value: " << check_trajectory_before_submission.value << std::endl;
-            std::cout << "Prevet index: " << check_trajectory_before_submission.index << std::endl;
+      case MediationLayerCode::Success:
+        // You probably won't need to do anything in response to Success.
+        break;
+      case MediationLayerCode::TimeBetweenPointsExceedsMaxTime: {
+        // Suppose your AP intends to submit a trajectory with a time that exceeds the maximum allowed time
+        // between points. The prevetter would catch this before you submit to the mediation layer.
+        std::cout << "Prevet: Shorten time between trajectory points."
+                  << std::endl;
+        std::cout << "Prevet: Time violation: "
+                  << check_trajectory_before_submission.value << std::endl;
+        break;
+      }
+      default:
+        std::cout << "Prevet code: "
+                  << static_cast<int>(check_trajectory_before_submission.code)
+                  << std::endl;
+        std::cout << "Prevet value: "
+                  << check_trajectory_before_submission.value << std::endl;
+        std::cout << "Prevet index: "
+                  << check_trajectory_before_submission.index << std::endl;
     }
 
     // Invoke the visualizer to see the proposed trajectory, which will be
@@ -285,8 +276,7 @@ namespace game_engine {
     // "ViewManager" implemented elsewhere in the game-engine code, but you
     // don't have full control over what it displays like you do with the
     // Student_game_engine_visualizer invoked below.
-    visualizer.drawTrajectory(trajectory);
-    trajectory_map[quad_name] = trajectory;
-    return trajectory_map;
+    // TODO: invoke visualizer, trajectory, and return
+    return 0;
   }
 }
